ae8343a3e382b4aee82fb030c33475da2bc920ee
baliram072x <baliramrathod0008@gmail.com>
2025-11-23 09:18:36 +0530

    update

--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -40,7 +40,7 @@ const Searchbar = () => {
           name="search-field"
           autoComplete="off"
           id="search-field"
-          className="flex-1 bg-transparent w-50 focus:border-b border-white lg:w-82 placeholder-gray-300 outline-none text-base text-white p-4"
+          className="flex-1 bg-transparent w-40 focus:border-b border-white lg:w-72 placeholder-gray-300 outline-none text-base text-white p-4"
           placeholder="Search"
           type="search"
           value={searchTerm}
